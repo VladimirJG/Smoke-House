173cb788bea32e898e6572ca8af212526b07b5b3
VladimirJG <vladimi85@gmail.com>
2024-01-16 09:05:42 +0300

    add dvaip create new(+ visual)

--- src/main/java/ru/danilov/Smoke/House/controllers/visual/DisposableVisualController.java
+++ src/main/java/ru/danilov/Smoke/House/controllers/visual/DisposableVisualController.java
@@ -1,11 +1,13 @@
 package ru.danilov.Smoke.House.controllers.visual;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.danilov.Smoke.House.models.Cigarettes;
+import ru.danilov.Smoke.House.models.DisposableVapes;
 import ru.danilov.Smoke.House.models.User;
 import ru.danilov.Smoke.House.services.DisposableService;
 
@@ -29,11 +31,26 @@ public class DisposableVisualController {
     }
 
     @GetMapping("/{id}")
-    public String getOneDV(@PathVariable("id") int id, Model model){
+    public String getOneDV(@PathVariable("id") int id, Model model) {
         model.addAttribute("oneDV", disposableService.findOne(id));
         List<User> usersList = disposableService.findOne(id).getUsersList();
         if (!usersList.isEmpty())
             model.addAttribute("uList", usersList);
         return "disposable_v/oneDV";
     }
+
+    @GetMapping("/newDV")
+    public String newDV(@ModelAttribute("dVape") DisposableVapes vape) {
+        return "disposable_v/newDV";
+    }
+
+    @PostMapping
+    public String createNewDV(@ModelAttribute("dVape") @Valid DisposableVapes vape, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "disposable_v/newDV";
+        }
+        disposableService.save(vape);
+        return "redirect:/disposable_v";
+    }
+
 }
