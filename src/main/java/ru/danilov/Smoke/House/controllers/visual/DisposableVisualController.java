ac58eeed15d964b9dae61f081447ef330b3b26d4
VladimirJG <vladimi85@gmail.com>
2024-01-16 08:35:04 +0300

    add dvaip show one(+ visual)

--- src/main/java/ru/danilov/Smoke/House/controllers/visual/DisposableVisualController.java
+++ src/main/java/ru/danilov/Smoke/House/controllers/visual/DisposableVisualController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.danilov.Smoke.House.models.User;
 import ru.danilov.Smoke.House.services.DisposableService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/disposable_v")
@@ -22,5 +26,14 @@ public class DisposableVisualController {
     public String getAllDV(Model model) {
         model.addAttribute("DV", disposableService.findAll());
         return "disposable_v/all";
+    }
+
+    @GetMapping("/{id}")
+    public String getOneDV(@PathVariable("id") int id, Model model){
+        model.addAttribute("oneDV", disposableService.findOne(id));
+        List<User> usersList = disposableService.findOne(id).getUsersList();
+        if (!usersList.isEmpty())
+            model.addAttribute("uList", usersList);
+        return "disposable_v/oneDV";
     }
 }
